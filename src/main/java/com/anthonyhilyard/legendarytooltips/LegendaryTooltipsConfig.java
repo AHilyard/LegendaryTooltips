9b3daaa63953288c44f98f5f09133f0e56a25eaf
Anthony Hilyard <anthony.hilyard@gmail.com>
2021-10-21 09:55:02 -0700

    Added border priorities option.

--- src/main/java/com/anthonyhilyard/legendarytooltips/LegendaryTooltipsConfig.java
+++ src/main/java/com/anthonyhilyard/legendarytooltips/LegendaryTooltipsConfig.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.HashMap;
 
 import com.anthonyhilyard.iceberg.util.ItemColor;
@@ -41,6 +43,8 @@ public class LegendaryTooltipsConfig
 
 	public final LongValue[] startColors = new LongValue[LegendaryTooltips.NUM_FRAMES];
 	public final LongValue[] endColors = new LongValue[LegendaryTooltips.NUM_FRAMES];
+
+	private final ConfigValue<List<? extends Integer>> framePriorities;
 	
 	private static final List<ConfigValue<List<? extends String>>> itemSelectors = new ArrayList<ConfigValue<List<? extends String>>>(LegendaryTooltips.NUM_FRAMES);
 
@@ -78,6 +82,11 @@ public class LegendaryTooltipsConfig
 			endColors[i] = build.defineInRange(String.format("level%d_end_color", i), 0xFF5A3A1DL, 0x00000000L, 0xFFFFFFFFL);
 		}
 
+		build.pop().comment(" Set border priorities here.  This should be a list of numbers that correspond to border levels, with numbers coming first being higher priority.");
+		build.push("priorities");
+
+		framePriorities = build.defineList("priorities", () -> IntStream.rangeClosed(0, LegendaryTooltips.NUM_FRAMES - 1).boxed().collect(Collectors.toList()), e -> ((int)e >= 0 && (int)e < LegendaryTooltips.NUM_FRAMES));
+
 		build.pop().comment(" Entry types:\n" + 
 							"   Item name - Use item name for vanilla items or include mod name for modded items.  Examples: minecraft:stick, iron_ore\n" +
 							"   Tag - $ followed by tag name.  Examples: $forge:stone or $planks\n" +
@@ -85,8 +94,7 @@ public class LegendaryTooltipsConfig
 							"   Rarity - ! followed by item's rarity.  This is ONLY vanilla rarities.  Examples: !uncommon, !rare, !epic\n" +
 							"   Item name color - # followed by color hex code, the hex code must match exactly.  Examples: #23F632\n" +
 							"   Display name - % followed by any text.  Will match any item with this text in its tooltip display name.  Examples: %[Uncommon]\n" +
-							"   Tooltip text - ^ followed by any text.  Will match any item with this text anywhere in the tooltip text (besides the name).  Examples: ^Rarity: Legendary" +
-							"   PLEASE NOTE: Lower frame levels take precedence, so if two levels match the same item, that item will display the lower numbered frame!");
+							"   Tooltip text - ^ followed by any text.  Will match any item with this text anywhere in the tooltip text (besides the name).  Examples: ^Rarity: Legendary");
 		build.push("definitions");
 
 		itemSelectors.clear();
@@ -114,71 +122,75 @@ public class LegendaryTooltipsConfig
 
 		for (int i = 0; i < LegendaryTooltips.NUM_FRAMES; i++)
 		{
-			for (String entry : itemSelectors.get(i).get())
+			if (i < framePriorities.get().size())
 			{
-				boolean found = false;
-				if (entry.equals(itemResourceLocation) || entry.equals(itemResourceLocation.replace("minecraft:", "")))
+				int frameIndex = framePriorities.get().get(i);
+				for (String entry : itemSelectors.get(frameIndex).get())
 				{
-					found = true;
-				}
-				else if (entry.startsWith("#"))
-				{
-					TextColor entryColor = TextColor.parseColor(entry);
-					if (entryColor.equals(ItemColor.getColorForItem(item, TextColor.fromRgb(0xFFFFFF))))
+					boolean found = false;
+					if (entry.equals(itemResourceLocation) || entry.equals(itemResourceLocation.replace("minecraft:", "")))
 					{
 						found = true;
 					}
-				}
-				else if (entry.startsWith("!"))
-				{
-					if (item.getRarity() == rarities.get(entry.substring(1)))
+					else if (entry.startsWith("#"))
 					{
-						found = true;
+						TextColor entryColor = TextColor.parseColor(entry);
+						if (entryColor.equals(ItemColor.getColorForItem(item, TextColor.fromRgb(0xFFFFFF))))
+						{
+							found = true;
+						}
 					}
-				}
-				else if (entry.startsWith("@"))
-				{
-					if (itemResourceLocation.startsWith(entry.substring(1) + ":"))
+					else if (entry.startsWith("!"))
 					{
-						found = true;
+						if (item.getRarity() == rarities.get(entry.substring(1)))
+						{
+							found = true;
+						}
 					}
-				}
-				else if (entry.startsWith("$"))
-				{
-					if (ItemTags.getAllTags().getTagOrEmpty(new ResourceLocation(entry.substring(1))).getValues().contains(item.getItem()))
+					else if (entry.startsWith("@"))
 					{
-						found = true;
+						if (itemResourceLocation.startsWith(entry.substring(1) + ":"))
+						{
+							found = true;
+						}
 					}
-				}
-				else if (entry.startsWith("%"))
-				{
-					if (item.getDisplayName().getString().contains(entry.substring(1)))
+					else if (entry.startsWith("$"))
 					{
-						found = true;
+						if (ItemTags.getAllTags().getTagOrEmpty(new ResourceLocation(entry.substring(1))).getValues().contains(item.getItem()))
+						{
+							found = true;
+						}
 					}
-				}
-				else if (entry.startsWith("^"))
-				{
-					Minecraft mc = Minecraft.getInstance();
-					List<Component> lines = item.getTooltipLines(mc.player, TooltipFlag.Default.ADVANCED);
-					String tooltipText = "";
-					
-					// Skip title line.
-					for (int n = 1; n < lines.size(); n++)
+					else if (entry.startsWith("%"))
 					{
-						tooltipText += lines.get(n).getString() + '\n';
+						if (item.getDisplayName().getString().contains(entry.substring(1)))
+						{
+							found = true;
+						}
 					}
-					if (tooltipText.contains(entry.substring(1)))
+					else if (entry.startsWith("^"))
 					{
-						found = true;
+						Minecraft mc = Minecraft.getInstance();
+						List<Component> lines = item.getTooltipLines(mc.player, TooltipFlag.Default.ADVANCED);
+						String tooltipText = "";
+						
+						// Skip title line.
+						for (int n = 1; n < lines.size(); n++)
+						{
+							tooltipText += lines.get(n).getString() + '\n';
+						}
+						if (tooltipText.contains(entry.substring(1)))
+						{
+							found = true;
+						}
 					}
-				}
 
-				if (found)
-				{
-					// Add to cache.
-					frameLevelCache.put(item, i);
-					return i;
+					if (found)
+					{
+						// Add to cache.
+						frameLevelCache.put(item, frameIndex);
+						return frameIndex;
+					}
 				}
 			}
 		}
