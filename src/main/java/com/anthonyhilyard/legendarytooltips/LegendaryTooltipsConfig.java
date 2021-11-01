8721e6bb2dfa1e098fe077f8c4981290bd390478
Anthony Hilyard <anthony.hilyard@gmail.com>
2021-11-01 14:29:50 -0700

    Added support for 6-digit color codes.

--- src/main/java/com/anthonyhilyard/legendarytooltips/LegendaryTooltipsConfig.java
+++ src/main/java/com/anthonyhilyard/legendarytooltips/LegendaryTooltipsConfig.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import com.anthonyhilyard.iceberg.util.ItemColor;
+import com.anthonyhilyard.iceberg.util.Selectors;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigData;
@@ -18,15 +18,7 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.JsonPrimitive;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.Registry;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextColor;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.TooltipFlag;
 
 @Config(name = Loader.MODID)
 public class LegendaryTooltipsConfig implements ConfigData
@@ -50,9 +42,9 @@ public class LegendaryTooltipsConfig implements ConfigData
 	public boolean shineEffect = true;
 
 	@Comment("Custom borders are broken into 16 \"levels\", with level 0 being intended for the \"best\" or \"rarest\" items. Only level 0 has a custom border built-in, but others can be added with resource packs.\n" +
-			 "The start border color (color at top of tooltip) of each levels' borders. Note that they can be entered as a lowercase 8-digit hex code in the format \"0xaarrggbb\" for convenience.")
+			 "The start border color (color at top of tooltip) of each levels' borders. Note that they can be entered as a lowercase hex code in the format \"0xaarrggbb\" or \"0xrrggbb\" for convenience.")
 	public Long[] startColors = Collections.nCopies(LegendaryTooltips.NUM_FRAMES, 0xFF996922L).toArray(new Long[0]);
-	@Comment("The end border color (color at bottom of tooltip) of each levels' borders. Note that they can be entered as a lowercase 8-digit hex code in the format \"0xaarrggbb\" for convenience.")
+	@Comment("The end border color (color at bottom of tooltip) of each levels' borders. Note that they can be entered as a lowercase hex code in the format \"0xaarrggbb\" or \"0xrrggbb\" for convenience.")
 	public Long[] endColors = Collections.nCopies(LegendaryTooltips.NUM_FRAMES, 0xFF5A3A1DL).toArray(new Long[0]);
 
 	@Comment("Set border priorities here.  This should be a list of numbers that correspond to border levels, with numbers coming first being higher priority.")
@@ -67,13 +59,6 @@ public class LegendaryTooltipsConfig implements ConfigData
 			 "  Display name - % followed by any text.  Will match any item with this text in its tooltip display name.  Examples: \"%[Uncommon]\"\n" +
 			 "  Tooltip text - ^ followed by any text.  Will match any item with this text anywhere in the tooltip text (besides the name).  Examples: \"^Legendary\"")
 	private List<List<String>> itemSelectors = Stream.of(List.of(List.of("!rare", "!epic")), Collections.nCopies(LegendaryTooltips.NUM_FRAMES - 1, List.of(""))).flatMap(List::stream).collect(Collectors.toList());
-
-	private static Map<String, Rarity> rarities = new HashMap<String, Rarity>() {{
-		put("common", Rarity.COMMON);
-		put("uncommon", Rarity.UNCOMMON);
-		put("rare", Rarity.RARE);
-		put("epic", Rarity.EPIC);
-	}};
 
 	private static Map<ItemStack, Integer> frameLevelCache = new HashMap<ItemStack, Integer>();
 
@@ -100,6 +85,21 @@ public class LegendaryTooltipsConfig implements ConfigData
 			convertedSelectors.add(convertedList);
 		}
 		itemSelectors = convertedSelectors.subList(Math.max(0, itemSelectors.size() - LegendaryTooltips.NUM_FRAMES), itemSelectors.size());
+
+		// Check for 6-digit color codes and convert them to 8-digit.
+		for (int i = 0; i < LegendaryTooltips.NUM_FRAMES; i++)
+		{
+			// If alpha is 0 but the color isn't 0x00000000, assume alpha is intended to be 0xFF.
+			// Only downside is if users want black borders they'd have to specify "0xFF000000".
+			if (startColors[i] > 0 && startColors[i] <= 0xFFFFFF)
+			{
+				startColors[i] = startColors[i] | (0xFF << 24);
+			}
+			if (endColors[i] > 0 && endColors[i] <= 0xFFFFFF)
+			{
+				endColors[i] = endColors[i] | (0xFF << 24);
+			}
+		}
 	}
 
 	public int getFrameLevelForItem(ItemStack item)
@@ -110,8 +110,6 @@ public class LegendaryTooltipsConfig implements ConfigData
 		}
 
 		// Check each level from 0 to 3 for matches for this item, from most specific to least.
-		String itemResourceLocation = Registry.ITEM.getKey(item.getItem()).toString();
-
 		for (int i = 0; i < LegendaryTooltips.NUM_FRAMES; i++)
 		{
 			if (i < framePriorities.length)
@@ -119,65 +117,7 @@ public class LegendaryTooltipsConfig implements ConfigData
 				int frameIndex = framePriorities[i];
 				for (String entry : itemSelectors.get(frameIndex))
 				{
-					boolean found = false;
-					if (entry.equals(itemResourceLocation) || entry.equals(itemResourceLocation.replace("minecraft:", "")))
-					{
-						found = true;
-					}
-					else if (entry.startsWith("#"))
-					{
-						TextColor entryColor = TextColor.parseColor(entry);
-						if (entryColor.equals(ItemColor.getColorForItem(item, TextColor.fromRgb(0xFFFFFF))))
-						{
-							found = true;
-						}
-					}
-					else if (entry.startsWith("!"))
-					{
-						if (item.getRarity() == rarities.get(entry.substring(1)))
-						{
-							found = true;
-						}
-					}
-					else if (entry.startsWith("@"))
-					{
-						if (itemResourceLocation.startsWith(entry.substring(1) + ":"))
-						{
-							found = true;
-						}
-					}
-					else if (entry.startsWith("$"))
-					{
-						if (ItemTags.getAllTags().getTagOrEmpty(new ResourceLocation(entry.substring(1))).getValues().contains(item.getItem()))
-						{
-							found = true;
-						}
-					}
-					else if (entry.startsWith("%"))
-					{
-						if (item.getDisplayName().getString().contains(entry.substring(1)))
-						{
-							found = true;
-						}
-					}
-					else if (entry.startsWith("^"))
-					{
-						Minecraft mc = Minecraft.getInstance();
-						List<Component> lines = item.getTooltipLines(mc.player, TooltipFlag.Default.ADVANCED);
-						String tooltipText = "";
-						
-						// Skip title line.
-						for (int n = 1; n < lines.size(); n++)
-						{
-							tooltipText += lines.get(n).getString() + '\n';
-						}
-						if (tooltipText.contains(entry.substring(1)))
-						{
-							found = true;
-						}
-					}
-
-					if (found)
+					if (Selectors.itemMatches(item, entry))
 					{
 						// Add to cache.
 						frameLevelCache.put(item, frameIndex);
@@ -191,40 +131,6 @@ public class LegendaryTooltipsConfig implements ConfigData
 		frameLevelCache.put(item, LegendaryTooltips.STANDARD);
 		return LegendaryTooltips.STANDARD;
 	}
-
-	// private static boolean validateCustomBorderEntry(String value)
-	// {
-	// 	// If this is a tag, check that it exists.
-	// 	if (value.startsWith("$"))
-	// 	{
-	// 		return ResourceLocation.isValidResourceLocation(value.substring(1));
-	// 	}
-	// 	// If this is a mod, check that mod is loaded.
-	// 	else if (value.startsWith("@"))
-	// 	{
-	// 		return true;
-	// 	}
-	// 	// If this is a rarity, make sure it's a valid one.
-	// 	else if (value.startsWith("!"))
-	// 	{
-	// 		return rarities.keySet().contains(value.substring(1).toLowerCase());
-	// 	}
-	// 	// If this is a hex color, ensure it's in a valid format.
-	// 	else if (value.startsWith("#"))
-	// 	{
-	// 		return TextColor.parseColor(value) != null;
-	// 	}
-	// 	// Text matches are always considered valid.
-	// 	else if (value.startsWith("%") || value.startsWith("^"))
-	// 	{
-	// 		return true;
-	// 	}
-	// 	// Otherwise it's an item, so just make sure it's a value resource location.
-	// 	else
-	// 	{
-	// 		return value == null || value == "" || ResourceLocation.isValidResourceLocation(value);
-	// 	}
-	// }
 
 	public int getCustomBorderStartColor(int level)
 	{
